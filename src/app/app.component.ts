6b287186b471a100fa329957e0071adf5f0d78ff
JeanEdouardMonnerville <jean-edouard.monnerville@etud.univ-jfc.fr>
2022-09-15 15:59:00 +0200

    INIT AJAX REQUEST

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+const urlEvent = "https://fhir.alliance4u.io/api/account/";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,3 +8,69 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'FPA';
 }
+
+
+function getEvent(idPatient:string){
+  let result : any[]; 
+  $.ajax(
+    {
+      type:"GET",
+      url:urlEvent,
+      contentType: 'application/json',
+      dataType:"json",
+      success:(data)=>{
+        for(let d of data){
+          if(d.subject.reference === idPatient && d.subject.type === "Patient"){
+            result.push(d);
+          }
+        }
+        return result;
+      },
+      error:(e)=>{
+        console.log(e)
+      }
+    }
+  )
+}
+
+function postEvent(newEvent:any){
+  $.ajax(
+    {
+      type:"POST",
+      url:urlEvent,
+      contentType: 'application/json',
+      data:JSON.stringify(newEvent),
+      success:(data)=>{
+        console.log("Envoi de l'événement réussi !");
+        return data;
+      },
+      error:(e)=>{
+        console.log(e)
+      }
+    }
+  )
+
+}
+
+function putEvent(newEvent:any){
+  $.ajax(
+    {
+      type:"PUT",
+      url:urlEvent+newEvent.id,
+      contentType: 'application/json',
+      data:JSON.stringify(newEvent),
+      success:(data)=>{
+        console.log("Modification de l'événement réussi !");
+        return data;
+      },
+      error:(e)=>{
+        console.log(e)
+      }
+    }
+  )
+}
+
+function postRDV(){
+
+}
+
