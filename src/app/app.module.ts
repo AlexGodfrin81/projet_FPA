8265f25207af447f16a326b2f679506340d45274
Alex Godfrin <neptuna1998@gmail.com>
2022-09-15 08:51:15 +0200

    retour app initiale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MedicamentComponent } from './medicament/medicament.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MedicamentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
