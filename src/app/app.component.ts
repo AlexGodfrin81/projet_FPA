e1e83c8cffa95a060b0f2b5b8e917448449f7933
Alex Godfrin <neptuna1998@gmail.com>
2022-09-21 10:20:24 +0200

    modif html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,67 +10,67 @@ export class AppComponent {
 }
 
 
-function getEvent(idPatient:string){
-  let result : any[]; 
-  $.ajax(
-    {
-      type:"GET",
-      url:urlEvent,
-      contentType: 'application/json',
-      dataType:"json",
-      success:(data)=>{
-        for(let d of data){
-          if(d.subject.reference === idPatient && d.subject.type === "Patient"){
-            result.push(d);
-          }
-        }
-        return result;
-      },
-      error:(e)=>{
-        console.log(e)
-      }
-    }
-  )
-}
+// function getEvent(idPatient:string){
+//   let result : any[]; 
+//   $.ajax(
+//     {
+//       type:"GET",
+//       url:urlEvent,
+//       contentType: 'application/json',
+//       dataType:"json",
+//       success:(data)=>{
+//         for(let d of data){
+//           if(d.subject.reference === idPatient && d.subject.type === "Patient"){
+//             result.push(d);
+//           }
+//         }
+//         return result;
+//       },
+//       error:(e)=>{
+//         console.log(e)
+//       }
+//     }
+//   )
+// }
 
-function postEvent(newEvent:any){
-  $.ajax(
-    {
-      type:"POST",
-      url:urlEvent,
-      contentType: 'application/json',
-      data:JSON.stringify(newEvent),
-      success:(data)=>{
-        console.log("Envoi de l'événement réussi !");
-        return data;
-      },
-      error:(e)=>{
-        console.log(e)
-      }
-    }
-  )
+// function postEvent(newEvent:any){
+//   $.ajax(
+//     {
+//       type:"POST",
+//       url:urlEvent,
+//       contentType: 'application/json',
+//       data:JSON.stringify(newEvent),
+//       success:(data)=>{
+//         console.log("Envoi de l'événement réussi !");
+//         return data;
+//       },
+//       error:(e)=>{
+//         console.log(e)
+//       }
+//     }
+//   )
 
-}
+// }
 
-function putEvent(newEvent:any){
-  $.ajax(
-    {
-      type:"PUT",
-      url:urlEvent+newEvent.id,
-      contentType: 'application/json',
-      data:JSON.stringify(newEvent),
-      success:(data)=>{
-        console.log("Modification de l'événement réussi !");
-        return data;
-      },
-      error:(e)=>{
-        console.log(e)
-      }
-    }
-  )
-}
+// function putEvent(newEvent:any){
+//   $.ajax(
+//     {
+//       type:"PUT",
+//       url:urlEvent+newEvent.id,
+//       contentType: 'application/json',
+//       data:JSON.stringify(newEvent),
+//       success:(data)=>{
+//         console.log("Modification de l'événement réussi !");
+//         return data;
+//       },
+//       error:(e)=>{
+//         console.log(e)
+//       }
+//     }
+//   )
+// }
 
-function postRDV(){
+// function postRDV(){
 
-}
+// }
 
